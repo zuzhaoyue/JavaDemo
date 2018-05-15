dddf03bb7b720d8735140de2203c6cabd249c714
Zhaoyue Zu <zhaoyue.zu@ele.me>
2018-05-15 15:12:32 +0800

    first push

---
+++ src/main/java/LoopStr.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zuzhaoyue on 18/5/8.
+ */
+public class LoopStr {
+    public static void test(String ...a){
+        System.out.println("========" + a);
+    }
+    public static void main(String[] args){
+//        test();
+        List l = new ArrayList<String>();
+        l.add("a");
+        boolean result = l.contains(null);
+        System.out.println("reslut:" + result);
+        /*String str="123454545456789";
+        Map<String ,Integer> map=new HashMap<String ,Integer>();
+
+        for(int i=0;i<str.length()-3;i++)//处理的字符串向后移动
+        {
+            int k=i;
+            String str1=str.substring(i,i+2);
+            String str2=str.substring(i+2);
+            while(str2.contains(str1))//后面的字串包含前面的
+            {
+                int cnt=1;
+                String strtemp=str2;
+                while(strtemp.contains(str1))
+                {
+                    cnt++;
+                    strtemp=strtemp.substring(strtemp.indexOf(str1)+str1.length());
+                    //被查找的字符串更新
+                }
+                if(!map.containsKey(str1))
+                    map.put(str1,cnt);//放入HashMap
+
+                str1=str.substring(i,++k+2);//”可能的重复串“扩展
+                str2=str.substring(k+2);
+            }
+        }
+
+        int j=0;//以下程序为了格式化输出
+        for(Object key:map.keySet())
+        {
+            j++;
+            System.out.print(key+":"+map.get(key)+"\t");
+            if(j%5==0)
+                System.out.println();
+        }*/
+    }
+}
---
+++ src/main/java/Recv.java
@@ -0,0 +1,38 @@
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by zuzhaoyue on 18/5/15.
+ */
+public class Recv {
+    private final static String QUEUE_NAME = "hello";
+    public static void main(String[] argv) throws java.io.IOException,
+            java.lang.InterruptedException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        //以下的defaultconsumer实现了consumer这个接口,这个接口被用来缓冲服务器推送过来的信息
+        //一开始的set up和刚刚的send.java里的相似:1.打开一个连接,2.声明一个队列（这个队列名要和刚刚的队列名相同）
+        //注意:我们在这里声明队列,因为我们可能在生产者之前开始消费
+        //我们告诉服务器从队列向我们传送消息,既然它会异步传送,我们以对象的形式提供一个回调,来缓冲这些消息,直到我们准备使用它们,这正是defaultconsumer做的事情
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        String  result = channel.basicConsume(QUEUE_NAME, true, consumer);
+        System.out.println("result:" + result);
+
+    }
+}
